1ecbcbb63804779eb0275b39b6abda4132da3393
Nabhye <72977665+Nabhye@users.noreply.github.com>
2022-03-24 14:48:30 +0530

    Add files via upload

---
+++ z_scores.py
@@ -0,0 +1,40 @@
+import csv
+import statistics
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import pandas as pd
+import random
+
+df = pd.read_csv("medium_data2.csv")
+data = df["reading_time"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data))
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+def setup():
+    mean_list = []
+    for i in range(0, 100):
+        set_of_means = random_set_of_mean(30)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+
+def show_fig(mean_list):
+    df = mean_list
+    fig = ff.create_distplot([df], ["temp"], show_hist=False)
+    fig.show()
+
+std_deviation = statistics.stdev(mean_list)
+mean = statistics.mean(mean_list)
+print("mean of sampling distribution:- ",mean)
+print("Standard deviation of sampling distribution:- ", std_deviation)
+
+
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
